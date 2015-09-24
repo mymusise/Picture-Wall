d0293dfe2e78587f45fa1cfea1a287feaf9c2029
mymusise <mymusise1@gmail.com>
2015-09-24 08:57:50 +0800

    A test of function.
    Signed-off-by: mymusise <mymusise1@gmail.com>

--- src/api.py
+++ src/api.py
@@ -1,3 +1,33 @@
+from  PIL  import Image
+'''
+Calculate a image color flag value and
+
+return the processed image
+'''
 def Calculate(image):
-    image = image.rotate(180)
-    return image
+	# image = image.rotate(180)
+	mean = Calculate_Color(image)
+	mask_image = Image.new("RGBA",image.size,mean)
+	base_image = Image.open("../resource/test1.png").resize(image.size)
+	'''
+		merge image 
+	'''
+	result = Image.blend(base_image,image,0.4)
+	return result
+	
+def Calculate_Color(image):
+	r_mean=0
+	g_mean=0
+	b_mean=0
+	x,y = image.size
+	for i in range(x):
+		for j in range(y):
+			rgb = image.getpixel((i,j))
+			r_mean+=int(rgb[0])
+			g_mean+=int(rgb[0])
+			b_mean+=int(rgb[0])
+	r_mean/=x*y
+	g_mean/=x*y
+	b_mean/=x*y
+	print (r_mean,g_mean,b_mean)
+	return (r_mean,g_mean,b_mean)
--- src/main.py
+++ src/main.py
@@ -1,67 +1,63 @@
-from  PIL  import Image
 from api import *
 '''
 Goble define
 '''
 Pixel_Byte_Define = 20
 
-'''
-Calculate a image color flag value and
-
-return the processed image
-'''
 
 
 def Combines(original_im):
-    original_x,original_y = original_im.size
-    '''
-    new a Image object and initialize
-    '''
-    small_x = int(original_x/Pixel_Byte_Define)
-    small_y = int(original_y/Pixel_Byte_Define)
-    wall_img = Image.new("RGB",
-                         (
-                          small_x*Pixel_Byte_Define,
-                          small_y*Pixel_Byte_Define
-                          ),
-                         "#ffffff")
-    for p_x in range(Pixel_Byte_Define):
-        for p_y in range(Pixel_Byte_Define):
-            current_coord = (
-                                p_x*small_x,
-                                p_y*small_y
-                             )
-            '''
-            crop current area
-            '''
-            small_img = original_im.crop(
-                                         (
-                                              current_coord[0],
-                                              current_coord[1],
-                                              current_coord[0]+small_x,
-                                              current_coord[1]+small_y
-                                          )
-                                        )
-            '''
-            get a image after Calculate
-            '''
-            small_img = Calculate(small_img)
-            '''
-            paste the ok image to current area
-            '''
-            wall_img.paste(small_img,current_coord)
-    return wall_img
+	original_x,original_y = original_im.size
+	'''
+	new a Image object and initialize
+	'''
+	small_x = int(original_x/Pixel_Byte_Define)
+	small_y = int(original_y/Pixel_Byte_Define)
+	wall_img = Image.new("RGB",
+						 (
+						  small_x*Pixel_Byte_Define,
+						  small_y*Pixel_Byte_Define
+						  ),
+						 "#ffffff")
+	for p_x in range(Pixel_Byte_Define):
+		for p_y in range(Pixel_Byte_Define):
+			current_coord = (
+								p_x*small_x,
+								p_y*small_y
+							 )
+			'''
+			crop current area
+			'''
+			small_img = original_im.crop(
+										 (
+											  current_coord[0],
+											  current_coord[1],
+											  current_coord[0]+small_x,
+											  current_coord[1]+small_y
+										  )
+										)
+			'''
+			get a image after Calculate
+			'''
+			small_img = Calculate(small_img)
+      
+			'''
+			paste the ok image to current area
+			'''
+			wall_img.paste(small_img,current_coord)
+	return wall_img
 
 
 def main():
-    im = Image.open('../resource/test1.png')
-    im = Combines(im)
-    im.show()
-    pass
+	im = Image.open('../resource/test3.jpg').convert("RGBA")
+	im = Combines(im)
+	im.show()
+	pass
+
 
 def test():
-    im = Image.open('../resource/test2.png')
-    # im = im.array(im)
+	im = Image.open('../resource/test2.png')
+	# im = im.array(im)
 if __name__ == '__main__':
-    main()
-    # test()
+	main()
+	# test()
