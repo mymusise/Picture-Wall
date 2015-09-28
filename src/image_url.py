14027d271a250875725451064178984e7c5fa355
mymusise <mymusise1@gmail.com>
2015-09-28 19:45:33 +0800

    add images for test
    Signed-off-by: mymusise <mymusise1@gmail.com>

---
+++ src/image_url.py
@@ -0,0 +1,14 @@
+cartoon = {
+	"red":"http://cn.bing.com/images/search?pq=%u52a8%u6f2b&sc=8-2&sp=6&sk=IM5&q=%E5%8A%A8%E6%BC%AB%E4%BA%BA%E7%89%A9&qft=+filterui:color2-FGcls_RED&FORM=R5IR8",
+	"orange":"http://www.bing.com/images/search?pq=%25u52a8%25u6f2b&sc=8-2&sp=6&sk=IM5&sid=854BC6B2ACE64AACB33B62122361D12D&sid=854BC6B2ACE64AACB33B62122361D12D&q=%E5%8A%A8%E6%BC%AB%E4%BA%BA%E7%89%A9&qft=+filterui:color2-FGcls_ORANGE&FORM=R5IR8&ajf=70",
+	"yellow":"http://www.bing.com/images/search?pq=%25u52a8%25u6f2b&sc=8-2&sp=6&sk=IM5&sid=854BC6B2ACE64AACB33B62122361D12D&sid=854BC6B2ACE64AACB33B62122361D12D&ajf=70&q=%E5%8A%A8%E6%BC%AB%E4%BA%BA%E7%89%A9&qft=+filterui:color2-FGcls_YELLOW&FORM=R5IR9",
+	"blue":"http://www.bing.com/images/search?pq=%25u52a8%25u6f2b&sc=8-2&sp=6&sk=IM5&sid=854BC6B2ACE64AACB33B62122361D12D&sid=854BC6B2ACE64AACB33B62122361D12D&ajf=70&q=%E5%8A%A8%E6%BC%AB%E4%BA%BA%E7%89%A9&qft=+filterui:color2-FGcls_BLUE&FORM=R5IR12",
+	"green":"http://www.bing.com/images/search?pq=%25u52a8%25u6f2b&sc=8-2&sp=6&sk=IM5&sid=854BC6B2ACE64AACB33B62122361D12D&sid=854BC6B2ACE64AACB33B62122361D12D&ajf=70&q=%E5%8A%A8%E6%BC%AB%E4%BA%BA%E7%89%A9&qft=+filterui:color2-FGcls_GREEN&FORM=R5IR11",
+	"cyan":"http://www.bing.com/images/search?pq=%25u52a8%25u6f2b&sc=8-2&sp=6&sk=IM5&sid=854BC6B2ACE64AACB33B62122361D12D&sid=854BC6B2ACE64AACB33B62122361D12D&ajf=70&q=%E5%8A%A8%E6%BC%AB%E4%BA%BA%E7%89%A9&qft=+filterui:color2-FGcls_TEAL&FORM=R5IR11",
+	"purple":"http://www.bing.com/images/search?pq=%25u52a8%25u6f2b&sc=8-2&sp=6&sk=IM5&sid=854BC6B2ACE64AACB33B62122361D12D&sid=854BC6B2ACE64AACB33B62122361D12D&ajf=70&q=%E5%8A%A8%E6%BC%AB%E4%BA%BA%E7%89%A9&qft=+filterui:color2-FGcls_PURPLE&FORM=R5IR13",
+	"pink":"http://www.bing.com/images/search?pq=%25u52a8%25u6f2b&sc=8-2&sp=6&sk=IM5&sid=854BC6B2ACE64AACB33B62122361D12D&sid=854BC6B2ACE64AACB33B62122361D12D&ajf=70&q=%E5%8A%A8%E6%BC%AB%E4%BA%BA%E7%89%A9&qft=+filterui:color2-FGcls_PINK&FORM=R5IR14",
+	"brown":"http://www.bing.com/images/search?pq=%25u52a8%25u6f2b&sc=8-2&sp=6&sk=IM5&sid=854BC6B2ACE64AACB33B62122361D12D&sid=854BC6B2ACE64AACB33B62122361D12D&ajf=70&q=%E5%8A%A8%E6%BC%AB%E4%BA%BA%E7%89%A9&qft=+filterui:color2-FGcls_BROWN&FORM=R5IR15",
+	"black":"http://www.bing.com/images/search?pq=%25u52a8%25u6f2b&sc=8-2&sp=6&sk=IM5&sid=854BC6B2ACE64AACB33B62122361D12D&sid=854BC6B2ACE64AACB33B62122361D12D&ajf=70&q=%E5%8A%A8%E6%BC%AB%E4%BA%BA%E7%89%A9&qft=+filterui:color2-FGcls_BLACK&FORM=R5IR16",
+	"white":"http://www.bing.com/images/search?pq=%25u52a8%25u6f2b&sc=8-2&sp=6&sk=IM5&sid=854BC6B2ACE64AACB33B62122361D12D&sid=854BC6B2ACE64AACB33B62122361D12D&ajf=70&q=%E5%8A%A8%E6%BC%AB%E4%BA%BA%E7%89%A9&qft=+filterui:color2-FGcls_WHITE&FORM=R5IR18",
+	"grey":"http://www.bing.com/images/search?pq=%25u52a8%25u6f2b&sc=8-2&sp=6&sk=IM5&sid=854BC6B2ACE64AACB33B62122361D12D&sid=854BC6B2ACE64AACB33B62122361D12D&ajf=70&q=%E5%8A%A8%E6%BC%AB%E4%BA%BA%E7%89%A9&qft=+filterui:color2-FGcls_GRAY&FORM=R5IR18"
+}
--- src/spider.py
+++ src/spider.py
@@ -2,41 +2,61 @@ import urllib
 import urllib2 
 from PIL import Image
 import StringIO 
+import random
+import string
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import image_url
+
 def delay(time):
 	for i in range(time):
 		for j in range(1000):
 			for k in range(500):
 				a=100*100
 
+def getRandomName(name_length):
+	return string.join(random.sample(['z','y','x','w','v','u','t','s','r','q','p','o','n','m','l','k','j','i','h','g','f','e','d','c','b','a'], name_length)).replace(' ','')
+
 class BingSpider():
-	url=["http://cn.bing.com/images/search?pq=%u52a8%u6f2b&sc=8-2&sp=6&sk=IM5&q=%E5%8A%A8%E6%BC%AB%E4%BA%BA%E7%89%A9&qft=+filterui:color2-FGcls_RED&FORM=R5IR8"]
+	urls=[
+			image_url.cartoon['purple'],
+			image_url.cartoon['pink'],
+			image_url.cartoon['brown'],
+			image_url.cartoon['black'],
+			image_url.cartoon['white'],
+			image_url.cartoon['grey']
+		]
 	driver = webdriver.Firefox()
-	load_page_length = 1
+	load_page_length = 2
 	def crawl(self):
-		self.driver.get(self.url[0])
-		self.loadmore(self.load_page_length)
-		self.getimage()
+		for url in self.urls:
+			self.driver.get(url)
+			self.loadmore(self.load_page_length)
+			self.getimage()
 		
 	def loadmore(self,time):
 		for j in range(time):
 			next_page = self.driver.find_elements_by_xpath("//a[@id='fbpgbt']")
-			for i in range(12):
+			for i in range(15):
 				next_page[0].send_keys(Keys.PAGE_DOWN)
 				delay(20)
 			more_page = self.driver.find_elements_by_xpath("//div[@class='mm_seemore']/a[@class='btn_seemore']")
-			more_page[0].click()
+			# more_page[0].click()
 
 	def getimage(self):
 		images = self.driver.find_elements_by_xpath("//div[@class='imgres']/div/a/img")
 		print len(images)
-		for img in images[:4]:
+		for img in images:
 			url = img.get_attribute('src')
 			print url
-			image_data =urllib2.urlopen(url).read()
-			img_file = Image.open(StringIO.StringIO(image_data))
-			img_file.show()
+			try:
+				image_data =urllib2.urlopen(url).read()
+				img_file = Image.open(StringIO.StringIO(image_data))
+				url = self.driver.current_url
+				color = url[url.find("FGcls_")+6:url.find("&",url.find("FGcls_"))].lower()
+				img_file.save('../resource/img.bing.com/cartoon/'+color+'/'+getRandomName(15)+'.jpg')
+			except:
+				pass
 
 
 import urllib 
